ea018ab15effe7d7cde39bd5fb27ba84e2620d99
neingeist <neingeist@bl0rg.net>
2014-03-09 23:53:01 +0100

    include trigonometric function values for a step size of pi/6

--- anki-sin-cos-table.py
+++ anki-sin-cos-table.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import OrderedDict
 from functools import reduce
 
 from sympy import latex, simplify
@@ -14,8 +15,12 @@ def as_csv(*values):
     return reduce(lambda a, b: a + ";" + b, values)
 
 
-xs = map(simplify,
-         [i * pi / 4 for i in range(-16, 16)])
+def deduplicate(iterable):
+    return list(OrderedDict.fromkeys(iterable))
+
+
+xs = sorted(deduplicate(map(simplify,
+                            [i * pi / 4 for i in range(-16, 16)] + [i * pi / 6 for i in range(-24, 24)])))
 
 for x in xs:
     for f in [sin, cos, tan]:
